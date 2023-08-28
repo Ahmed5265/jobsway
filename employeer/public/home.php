f9629166bd2db0393295b253c52d061b7908d604
maryam146 <maryam2112d@aptechorangi.com>
2023-08-29 04:07:21 +0500

    okay

---
+++ employeer/public/home.php
@@ -0,0 +1,207 @@
+<?php include 'db-config.php'?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Connect Plus</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="../content/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="../content/vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="../content/vendors/css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="../content/vendors/font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="../content/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="../content/css/style.css">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="../content/images/favicon.png" />
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_navbar.html -->
+      <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+        <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+          <h1>Finate</h1>
+          <!-- <a class="navbar-brand brand-logo-mini" href="index.html"><img href="../content/images/logo-light.png" alt= /></a> -->
+        </div>
+        <div class="navbar-menu-wrapper d-flex align-items-stretch">
+          <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+            <span class="mdi mdi-menu"></span>
+          </button>
+          <div class="search-field d-none d-xl-block">
+            <form class="d-flex align-items-center h-100" action="#">
+              <div class="input-group">
+                <div class="input-group-prepend bg-transparent">
+                  <i class="input-group-text border-0 mdi mdi-magnify"></i>
+                </div>
+                <input type="text" class="form-control bg-transparent border-0" placeholder="Search products">
+              </div>
+            </form>
+          </div>
+          <ul class="navbar-nav navbar-nav-right">
+        
+         
+            <li class="nav-item nav-language  d-none d-md-block">
+              <a class="nav-link " id="languageDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
+                <div class="nav-language-icon">
+                  <i class="flag-icon flag-icon-us" title="us" id="us"></i>
+                </div>
+                <div class="nav-language-text">
+                  <p class="mb-1 text-black">English</p>
+                </div>
+              </a>
+              
+            </li>
+              </a>
+         
+            </li>
+            <li class="nav-item">
+              <a class="nav-link count-indicator dropdown-toggle" id="" href="#" data-toggle="" aria-expanded="false">
+                <i class="mdi mdi-email-outline"></i>
+                <span class="count-symbol bg-success"></span>
+              </a>
+              <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="messageDropdown">
+                <h6 class="p-3 mb-0 bg-primary text-white py-4">Messages</h6>
+               
+                </a>
+               
+                
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item preview-item">
+                  <div class="preview-thumbnail">
+                    <img href="../content/images/faces/face3.jpg" alt="image" class="profile-pic">
+                  </div>
+              
+                </a>
+                <div class="dropdown-divider"></div>
+                <h6 class="p-3 mb-0 text-center">4 new messages</h6>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">
+                <i class="mdi mdi-bell-outline"></i>
+                <span class="count-symbol bg-danger"></span>
+              </a>
+              <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
+                <h6 class="p-3 mb-0 bg-primary text-white py-4">Notifications</h6>
+              
+                 
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item preview-item">
+                  <div class="preview-thumbnail">
+                    <div class="preview-icon bg-warning">
+                      <i class="mdi mdi-settings"></i>
+                    </div>
+                  </div>
+                 
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item preview-item">
+                  <div class="preview-thumbnail">
+                    <div class="preview-icon bg-info">
+                      <i class="mdi mdi-link-variant"></i>
+                    </div>
+                  </div>
+                  <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
+                    <h6 class="preview-subject font-weight-normal mb-1">Launch Admin</h6>
+                    <p class="text-gray ellipsis mb-0"> New admin wow! </p>
+                  </div>
+                </a>
+                <div class="dropdown-divider"></div>
+                <h6 class="p-3 mb-0 text-center">See all notifications</h6>
+              </div>
+              <li class="nav-item sidebar-user-actions">
+              <div class="sidebar-user-menu">
+                <a href="#" class="nav-link"><i class="mdi mdi-logout menu-icon"></i>
+                  <span class="menu-title">Log Out</span></a>
+              </div>
+            </li>
+            </li>
+          </ul>
+          <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+            <span class="mdi mdi-menu"></span>
+          </button>
+        </div>
+      </nav>
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_sidebar.html -->
+        <nav class="sidebar sidebar-offcanvas" id="sidebar">
+          <ul class="nav">
+            <li class="nav-item nav-category"></li>
+            <li class="nav-item">
+              <a class="nav-link" href="?page=index.php">
+                <span class="icon-bg"><i class="mdi mdi-cube menu-icon"></i></span>
+                <span class="menu-title">Dashboard</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="?page=job_post.php">
+                <span class="icon-bg"><i class="mdi mdi-contacts menu-icon"></i></span>
+                <span class="menu-title">post job</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="?page=job_list.php">
+                <span class="icon-bg"><i class="mdi mdi-format-list-bulleted menu-icon"></i></span>
+                <span class="menu-title"></span>
+              </a>
+            </li>
+         
+          </ul>
+        </nav>
+
+        <?php
+  
+
+  if(isset($_GET["page"]))
+  {
+
+    $pagename = $_GET["page"];
+
+    include($pagename);
+
+  }else{
+
+    include("index.php");
+  }
+
+
+
+  ?>
+
+
+</div>
+
+
+
+          <!-- partial -->
+
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script href="../contnt/vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script href="../contnt/vendors/chart.js/Chart.min.js"></script>
+    <script href="../contnt/vendors/jquery-circle-progress/js/circle-progress.min.js"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script href="../contnt/js/off-canvas.js"></script>
+    <script href="../contnt/js/hoverable-collapse.js"></script>
+    <script href="../contnt/js/misc.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <script href="../contnt/js/dashboard.js"></script>
+    <!-- End custom js for this page -->
+  </body>
+</html>
---
+++ employeer/public/job_post.php
@@ -0,0 +1,135 @@
+<?php include 'db-config.php'?>
+
+
+<div class="main-panel">
+          <div class="content-wrapper">
+            <div class="page-header">
+            
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                </ol>
+              </nav>
+             </div>
+
+                <div class="col-12 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    <h4 class="card-title">Post a Job</h4>
+                    <p class="card-description"> Add Employeer </p>
+                    <form class="forms-sample" method="post" enctype="multipart/form-data" >
+                      <div class="form-group">
+                        <label for="exampleInputName1">Name</label>
+                        <input type="text" class="form-control" name="name" id="exampleInputName1" placeholder="Name" required>
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputName1">Password</label>
+                        <input type="password" class="form-control" name="pass" id="exampleInputName1" placeholder="Name" required>
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputEmail3">Email address</label>
+                        <input type="email" class="form-control" name="email" id="exampleInputEmail3" placeholder="Email" required>
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputPassword4">Loaction</label>
+                        <select class="form-control" name="loc" id="" >
+                                            <option value="karachi">Karachi</option>
+    <option value="lahore">Lahore</option>
+    <option value="islamabad">Islamabad</option>
+    <option value="rawalpindi">Rawalpindi</option>
+    <option value="faisalabad">Faisalabad</option>
+    <option value="multan">Multan</option>
+    <option value="peshawar">Peshawar</option>
+    <option value="quetta">Quetta</option>
+    <option value="gujranwala">Gujranwala</option>
+    <option value="sialkot">Sialkot</option>
+    <option value="bahawalpur">Bahawalpur</option>
+    <option value="abbottabad">Abbottabad</option>
+    <option value="sargodha">Sargodha</option>
+    <option value="sukkur">Sukkur</option>
+    <option value="jhang">Jhang</option>
+    <option value="sheikhupura">Sheikhupura</option>
+    <option value="rahim-yar-khan">Rahim Yar Khan</option>
+    <option value="gujrat">Gujrat</option>
+    <option value="kasur">Kasur</option>
+    <option value="mardan">Mardan</option>
+    <option value="larkana">Larkana</option>
+    <option value="sahiwal">Sahiwal</option>
+    <option value="okara">Okara</option>
+    <option value="wah-cantt">Wah Cantt</option>
+    <option value="dera-ghazi-khan">Dera Ghazi Khan</option>
+    <option value="mirpur-khas">Mirpur Khas</option>
+    <option value="nawabshah">Nawabshah</option>
+    <option value="mingora">Mingora</option>
+    <option value="bannu">Bannu</option>
+    <option value="kamoke">Kamoke</option>
+    <option value="hafizabad">Hafizabad</option>
+    <option value="kohat">Kohat</option>
+    <option value="jacobabad">Jacobabad</option>
+    <option value="mirpur">Mirpur</option>
+    <option value="saddiqabad">Saddiqabad</option>
+    <option value="gujranwala">Gujranwala</option>
+    <option value="jhelum">Jhelum</option>
+    <option value="dera-ismail-khan">Dera Ismail Khan</option>
+    <option value="bhakkar">Bhakkar</option>
+    <option value="chakwal">Chakwal</option>
+    <option value="khairpur">Khairpur</option>
+    <option value="haripur">Haripur</option>
+    <option value="matiari">Matiari</option>
+    <option value="nowshera">Nowshera</option>
+    <option value="jalalpur-jattan">Jalalpur Jattan</option>
+    <option value="dera-murad-jamali">Dera Murad Jamali</option>
+                                                </select> 
+                      </div> 
+                      <div class="form-group">
+                        <label for="exampleInputPassword4">Contact no</label>
+                        <input type="num" value="92" maxlength="12" class="form-control" name="contact"  placeholder="Contact no" required>
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputCity1">Since</label>
+                        <input type="month" class="form-control" name="since"  placeholder="Since" required>
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleTextarea1">Description</label>
+                        <textarea class="form-control" name="desc" id="exampleTextarea1" rows="4" placeholder="Description" required></textarea>
+                      </div>
+                      <button type="submit" name="btn" class="btn btn-primary mr-2">Submit</button>
+                     
+                    </form>
+                  </div>
+                  </div>
+                  </div>
+                
+
+                  
+<?php
+if(isset($_POST["btn"]))
+{
+
+
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $since = $_POST["since"];
+    $location = $_POST["loc"];
+    $contact = $_POST["contact"];
+    $description = $_POST["desc"];
+    $pass = $_POST["pass"];
+
+
+
+
+
+    $sql = "insert into company(company_id,name,email,contact,since,location,description,Verified_by_admin,role_id,company_created_date_time,password) VALUES ('','$name','$email','$contact','$since','$location','$description','','2','','$pass')";
+
+    
+    if( mysqli_query($con,$sql)){
+
+      echo "<script>
+         alert('new Employeer add successfully!!');
+         window.location.href='?page=vendor_list.php';
+        </script>";
+
+         }
+}
+?>
+           
